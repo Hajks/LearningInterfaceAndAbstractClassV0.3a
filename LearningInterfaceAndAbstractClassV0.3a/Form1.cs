ac588a9fae5e20931bb97ca5ff9b7d8100b89dfe
Hajk <tunczorx@gmail.com>
2018-04-13 09:39:51 +0200

    A new portion of information about interfaces.

--- LearningInterfaceAndAbstractClassV0.3a/Form1.cs
+++ LearningInterfaceAndAbstractClassV0.3a/Form1.cs
@@ -15,6 +15,14 @@ namespace LearningInterfaceAndAbstractClassV0._3a
         public Form1()
         {
             InitializeComponent();
+            StingPatrol biff = new StingPatrol();
+            NectarCollector bertha = new NectarCollector();
+            IStingPatrol defender = biff; //We can add new reference only to class which implement this type of interface 
+            INectarCollector cutiePie = bertha; // Now, we have two references to our object, so even if we set up bertha to null, our garbage collector will leave our object NectarCollector alive because there is still cutiePie reference.
+            bertha = null;
+            INectarCollector gatherer = new NectarCollector(); // So currently our heap looks like: 2 objects NectarCollector and 1 object StingPatrol. Cool!
+            
+
         }
     }
 }
